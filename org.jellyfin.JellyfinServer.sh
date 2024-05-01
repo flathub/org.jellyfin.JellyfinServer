12259a7a7fce4fd4b8d0d8c6c6c797e40df0276b
hdL6c <107304850+istori1@users.noreply.github.com>
2024-04-30 23:34:18 -0400

    Update org.jellyfin.JellyfinServer.sh

--- org.jellyfin.JellyfinServer.sh
+++ org.jellyfin.JellyfinServer.sh
@@ -5,7 +5,7 @@ PORT=8096
 if ! curl http://localhost:$PORT > /dev/null 2>&1; then
   (sleep 10 && xdg-open http://localhost:$PORT) &
   mkdir -p ~/.var/app/org.jellyfin.JellyfinServer/media
-  jellyfin
+  exec jellyfin "$@"
 else
   xdg-open http://localhost:$PORT
 fi
